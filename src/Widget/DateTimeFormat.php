e9a98f356ed6d3569beadcaffd60cce786f2f306
Igor Maliy <imaliy.filsh@gmail.com>
2022-06-20 01:14:50 +0300

    Development

--- src/Widget/DateTimeFormat.php
+++ src/Widget/DateTimeFormat.php
@@ -2,23 +2,66 @@
 
 namespace Mailery\Web\Widget;
 
+use Mailery\Common\Field\Timezone;
+use Mailery\User\Service\CurrentUserService;
 use Yiisoft\Widget\Widget;
-use \DateTime;
 use \DateTimeImmutable;
 
 final class DateTimeFormat extends Widget
 {
 
     /**
-     * @var DateTime|DateTimeImmutable
+     * @var string
      */
-    private DateTime|DateTimeImmutable $dateTime;
+    private string $format = 'Y-m-d H:i:s';
 
     /**
-     * @param DateTime|DateTimeImmutable $dateTime
+     * @var Timezone
+     */
+    private Timezone $timezone;
+
+    /**
+     * @var DateTimeImmutable
+     */
+    private DateTimeImmutable $dateTime;
+
+    /**
+     * @param CurrentUserService $currentUser
+     */
+    public function __construct(CurrentUserService $currentUser)
+    {
+        $this->timezone = $currentUser->getUser()->getTimezone();
+    }
+
+    /**
+     * @param string $format
      * @return self
      */
-    public function dateTime(DateTime|DateTimeImmutable $dateTime): self
+    public function format(string $format): self
+    {
+        $new = clone $this;
+        $new->format = $format;
+
+        return $new;
+    }
+
+    /**
+     * @param Timezone $timezone
+     * @return self
+     */
+    public function timezone(Timezone $timezone): self
+    {
+        $new = clone $this;
+        $new->timezone = $timezone;
+
+        return $new;
+    }
+
+    /**
+     * @param DateTimeImmutable $dateTime
+     * @return self
+     */
+    public function dateTime(DateTimeImmutable $dateTime): self
     {
         $new = clone $this;
         $new->dateTime = $dateTime;
@@ -31,6 +74,8 @@ final class DateTimeFormat extends Widget
      */
     public function run(): string
     {
-        return $this->dateTime->format('Y-m-d H:i:s');
+        return $this->dateTime
+            ->setTimeZone(new \DateTimeZone($this->timezone->getValue()))
+            ->format($this->format);
     }
 }
